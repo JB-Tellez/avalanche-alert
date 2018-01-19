04429423b1e2bdc6fd82853774ba819d9f8b9662
JB Tellez <jb.tellez@gmail.com>
2018-01-19 00:18:00 -0800

    initial submit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const superagent = require('superagent')
+const app = express()
+const NWAC_URL = 'http://www.nwac.us/api/v2'
+const PORT = process.env.PORT || 3000
+    
+app.use(express.static('./public'))
+
+app.get('/forecasts/:zone', (req, res) => { 
+    
+    const url = NWAC_URL + '/avalanche-region-forecast/?format=json&zone=' +  req.params.zone
+    
+    superagent
+        .get(url)
+        .then(data => res.send(JSON.parse(data.text).objects))
+})
+
+app.get('/zones', (req, res) => {
+    superagent
+        .get(NWAC_URL + '/zone/?format=json')
+        .then(data => res.send(JSON.parse(data.text).objects))
+})
+
+app.listen(PORT, () => console.log('Server listening on port', PORT))
